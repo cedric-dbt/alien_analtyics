0d98bb7498392267ec8eb426714d55e4ab5903bb
Cedric Turner <cedric.turner@fishtownanalytics.com>
2025-09-17 21:42:09 +0000

    creating staging model for ufo_sightings

--- models/staging/stg_ufo_sightings.sql
+++ models/staging/stg_ufo_sightings.sql
@@ -1,1 +1,17 @@
-select * from {{ source('cedric_demo_data', 'ufo_sightings') }}
+with ufo_sightings as (
+    select
+        datetime,
+        city,
+        state,
+        country,
+        shape,
+        duration_seconds as duration,
+        floor(duration_seconds / 3600) as duration_hours,
+        floor((duration_seconds % 3600) / 60) as duration_minutes,
+        comments,
+        latitude,
+        longitude
+    from {{ source('ufo', 'ufo_sightings_scrubbed') }}
+)
+
+select * from ufo_sightings
